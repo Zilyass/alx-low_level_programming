fd489b3654bea3f9fd5126f95e6e91438c6b1f12
Zilyass <zilyasw3@gmail.com>
2023-03-25 11:05:43 -0700

    fix5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
- *main - print message
- *return always (0)
- **/
+/**
+ *main - this is the main function
+ *
+ *Return: always 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
